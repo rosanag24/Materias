a206f9be021eb3ce0e1b71bdd10af2cb48670a10
Constanza Abarca <costi.abarca@gmail.com>
2018-11-08 01:29:16 -0400

    lista la tabla (sencilla) de materias

--- posts/views.py
+++ posts/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 from posts.models import *
 
 def index(request):
+	materias = Asignatura.objects.all()
 	dpto = Departamento.objects.filter(codigoDepartamento = "0800")[0]
-	return render(request, 'index.html', {'dpto': dpto.nombreDepartamento})
+	return render(request, 'index.html', {'materias': materias, 'dpto':dpto})
